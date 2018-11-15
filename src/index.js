e6ac03f6679a7550230f709a2f6453133933287c
Andrei Lenkov <andrei_lenkov@epam.com>
2018-11-15 18:06:43 +0300

    feature: add functionality - add list of channels

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
+const API_KEY = 'b7898b8ae1f042849321a38b58c68df0';
+
 window.onload = function() {
-    const text = document.getElementById('output').innerHTML;
-    alert(text);
+    const url = `https://newsapi.org/v2/sources?apiKey=${API_KEY}`;
+    const req = new Request(url);
+    const textField = document.getElementById('output');
+
+    fetch(req)
+        .then(response => response.json())
+        .then(answer => {
+            const chanelsList = getChanelsList(answer.sources);
+            const ul = transformToHTMLChanelsList(chanelsList);
+            textField.appendChild(ul);
+        });
 };
 
-const API_KEY = 'b7898b8ae1f042849321a38b58c68df0';
+function getChanelsList(newsSources) {
+    return newsSources.map(source => source.id);
+}
+
+function transformToHTMLChanelsList(chanels) {
+    const ul = document.createElement('ul');
+    chanels.forEach(chanel => {
+        const li = document.createElement('li');
+        li.innerHTML = chanel;
+        ul.appendChild(li);
+    });
+    return ul;
+}
