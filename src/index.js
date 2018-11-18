11c61c3ec1ff8c927cff191fca968bf004a1792b
AndyArt <lenkov.andrei.89@gmail.com>
2018-11-18 18:31:25 +0300

    feature: add render functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const API_KEY = 'b7898b8ae1f042849321a38b58c68df0';
+const pageSize = 10;
 let selectedChanel;
 
 window.onload = function() {
@@ -19,6 +20,7 @@ function getChannelsList(newsSources) {
 
 function setChannelsNamesAsSelectOptions(channelsNames) {
     const select = document.getElementById('channel-select');
+    select.innerHTML = '';
     channelsNames.forEach(chanelName => {
         const option = document.createElement('option');
         option.innerHTML = chanelName;
@@ -31,6 +33,10 @@ function setChannelsNamesAsSelectOptions(channelsNames) {
 
 function downloadNews() {
     applySelectedChannel();
+    const request = formRequest(API_KEY, selectedChanel);
+    fetch(request)
+        .then(response => response.json())
+        .then(answer => renderNews(answer.articles));
 }
 
 function applySelectedChannel() {
@@ -39,6 +45,107 @@ function applySelectedChannel() {
 }
 
 function formRequest(apiKey, channelName='') {
-    const url = `https://newsapi.org/v2/${channelName === '' ? 'sources' : 'everything'}?${channelName === '' ? '' : `sources=${channelName}`}apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/${channelName === '' ? 'sources' : 'everything'}?${channelName === '' ? '' : `sources="${channelName}"&pageSize=${pageSize}`}&apiKey=${apiKey}`;
     return new Request(url);
+}
+
+function renderNews(articlesNews) {
+    console.log(articlesNews);
+    const articleContainer = document.getElementById('output');
+    let docFragment = document.createDocumentFragment();
+
+    articlesNews.forEach(articleNews => {
+        const articleHTML = document.createElement('article');
+        const h2 = document.createElement('h2');
+        const figure = document.createElement('figure');
+        const img = (articleNews.urlToImage !== null) ? document.createElement('img') : null;
+        const figcaption = document.createElement('figcaption');
+        const footer = document.createElement('footer');
+        const time = document.createElement('time');
+        const spanAuthor = document.createElement('span');
+        const spanSource = document.createElement('span');
+        const div = document.createElement('div');
+        const p = (articleNews.content !== null) ? document.createElement('p') : null;
+
+        h2.innerHTML = articleNews.title;
+        if (img !== null) {
+            img.setAttribute('src', articleNews.urlToImage);
+            img.setAttribute('width', 200);
+        }
+        figcaption.innerHTML = articleNews.description;
+        time.innerHTML = formatTimeToReadable(articleNews.publishedAt);
+        time.setAttribute('datetime', articleNews.publishedAt);
+        spanAuthor.innerHTML = articleNews.author;
+        spanSource.innerHTML = articleNews.source.name;
+        if (p !== null) {
+            p.innerHTML = articleNews.content;
+        }
+
+        div.appendChild(spanAuthor);
+        div.appendChild(spanSource);
+        footer.appendChild(time);
+        footer.appendChild(div);
+        articleHTML.appendChild(h2);
+        if (img !== null) {
+            figure.appendChild(img);
+        }
+        figure.appendChild(figcaption);
+        articleHTML.appendChild(figure);
+        if (p !== null) {
+            articleHTML.appendChild(p);
+        }
+        articleHTML.appendChild(footer);
+
+        docFragment.appendChild(articleHTML);
+    });
+
+    articleContainer.appendChild(docFragment);
+}
+
+function formatTimeToReadable(timeInternationalFormat) {
+    return timeInternationalFormat.split('T').join(' ').split(0, timeInternationalFormat.lastIndexOf(':'));
+}
+
+function createArticle(articleNews) {
+    const articleHTML = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const figure = document.createElement('figure');
+    const img = (articleNews.urlToImage !== null) ? document.createElement('img') : null;
+    const figcaption = document.createElement('figcaption');
+    const footer = document.createElement('footer');
+    const time = document.createElement('time');
+    const spanAuthor = document.createElement('span');
+    const spanSource = document.createElement('span');
+    const div = document.createElement('div');
+    const p = (articleNews.content !== null) ? document.createElement('p') : null;
+
+    h2.innerHTML = articleNews.title;
+    if (img !== null) {
+        img.setAttribute('src', articleNews.urlToImage);
+    }
+    figcaption.innerHTML = articleNews.description;
+    time.innerHTML = formatTimeToReadable(articleNews.publishedAt);
+    time.setAttribute('datetime', articleNews.publishedAt);
+    spanAuthor.innerHTML = articleNews.author;
+    spanSource.innerHTML = articleNews.source.name;
+    if (p !== null) {
+        p.innerHTML = articleNews.content;
+    }
+
+    div.appendChild(spanAuthor);
+    div.appendChild(spanSource);
+    footer.appendChild(time);
+    footer.appendChild(div);
+    articleHTML.appendChild(h2);
+    if (img !== null) {
+        figure.appendChild(img);
+    }
+    figure.appendChild(figcaption);
+    articleHTML.appendChild(figure);
+    if (p !== null) {
+        articleHTML.appendChild(p);
+    }
+    articleHTML.appendChild(footer);
+
+    return articleHTML;
 }
