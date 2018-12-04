4f62c140fd6c10185c486787073c5ea736f6cf66
AndyArt <lenkov.andrei.89@gmail.com>
2018-12-05 00:23:46 +0300

    feature/ view implemented

--- src/view/view.js
+++ src/view/view.js
@@ -1,16 +1,58 @@
+const INPUT = 'INPUT';
+
 export const view = {
-    button: '',
     navigation: null,
-    article: '',
     divContainer: '',
     init: () => {
-        this.button = `<input type="submit">`;
         this.navigation = document.getElementById('buttons');
-        this.article = `<article class="page-header_selection"></article>`;
         this.divContainer = document.getElementById('output');
+
+        this.navigation.addEventListener('click', event => {
+            if (event.target.tagName === INPUT) {
+                controller.changeNews(event.target.value);
+            }
+        });
+
         this.render();
     },
-    render: () => {
 
+    renderArticls: () => {
+        this.divContainer.innerHTML = '';
+
+        const fragment = document.createDocumentFragment();
+
+        controller.getArticls().forEach(article => {
+            const articleHTML = document.createElement('article');
+            articleHTML.innerHTML = `<a class="news_link" href="${article.url}">
+											<div class="news_image-wrapper">
+												<div class="news_image"></div>
+												<time class="news_pub-time" pubdate="${article.publishedAt}">${this.formatTimeToReadable(article.publishedAt)}</time>
+											</div>
+											<h2  class="news_headline">${article.title}</h2>
+										</a>`;
+            fragment.appendChild(articleHTML);
+        })
+
+        this.divContainer.appendChild(fragment);
+    },
+
+    renderButtons: () => {
+        this.navigation.innerHTML = '';
+
+        const fragment = document.createDocumentFragment();
+
+        controller.getButtons().forEach(btnText => {
+            const buttonHTML = document.createElement('input');
+            buttonHTML.setAttribute('type', 'submit');
+            buttonHTML.value = btnText;
+            fragment.appendChild(buttonHTML);
+        })
+
+        this.divContainer.appendChild(fragment);
+    },
+
+    render: () => {
+        this.renderArticls();
+        this.renderButtons();
     }
 }
