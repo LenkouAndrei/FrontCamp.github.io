379ca8562efffdfc1f477c8ed5e5987033b05beb
AndyArt <lenkov.andrei.89@gmail.com>
2019-01-20 16:32:10 +0300

    feature/ nodejs with mocked data

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
 const express = require('express')
 const app = express()
-const news = require('./news.json')
+const routes = require('./routes')
 const port = 3000
 
-app.get('/', function (req, res) {
-  console.log('index', { title: '111', message: 'Hello NodeJS' });
-})
+app.use('/', routes);
 
-app.get('/news', function (req, res) {
-  console.log(news);
-})
-
-app.get('/news/:id', function (req, res) {
-  const newsId = req.params.id;
-  if (news[newsId]) {
-    console.log(news[newsId]);
-  } else {
-    console.log('Error');
-  }
-})
-
-app.post('/news', (req, res) => {
-  res.status(200).json({ type: req.method });
-});
-
-app.put('/news/:id', (req, res) => {
-  let blogId = req.params.id;
-  res.status(200).json({ type: req.method, id: blogId });
-});
-
-app.delete('/news/:id', (req, res) => {
-  let blogId = req.params.id;
-  res.status(200).json({ type: req.method, id: blogId });
-});
-
-routes.use(function (req, res) {
-  if (res.status(404)) {
-    console.log('Error: Page not found')
-  }
-})
+app.set('view engine', 'pug');
 
 app.listen(port, (err) => {
   if (err) {
---
+++ routes.js
@@ -0,0 +1,60 @@
+const routes = require('express').Router();
+const news = require('./news.json');
+let logger = require('./logger');
+
+routes.get('/', function (req, res) {
+    res.status(200).render('index', { title: '111', message: 'Hello NodeJS' });
+    log(req);
+})
+
+routes.get('/news', function (req, res) {
+    let newsContent = Object.values(news);
+    let newsTitles = newsContent.map(item => item.title);
+    console.dir({ title: 'News', news: newsTitles, urls: Object.keys(news) });
+    res.status(200).render('allNews', { title: 'News', news: newsTitles, urls: Object.keys(news) });
+    log(req);
+});
+
+routes.post('/news', function (req, res) {
+    res.status(200).json({ type: req.method });
+    log(req);
+});
+
+routes.get('/news/:id', function (req, res) {
+    let newsId = req.params.id;
+    let currentNews = news[newsId];
+    if (!currentNews) {
+        res.status(404).render('pageNotFound', { title: 'Error', message: 'There is no news with such id' })
+    } else {
+        res.status(200).render('news', {
+            title: newsId,
+            header: currentNews.title,
+            text: currentNews.text,
+            date: currentNews.date
+        });
+    }
+    log(req);
+});
+
+routes.put('/news/:id', function (req, res) {
+    let newsId = req.params.id;
+    res.status(200).json({ type: req.method, id: newsId });
+    log(req);
+});
+
+routes.delete('/news/:id', function (req, res) {
+    let newsId = req.params.id;
+    res.status(200).json({ type: req.method, id: newsId });
+    log(req);
+});
+
+let log = (req) => {
+    logger.log({
+        level: 'info',
+        url: req.headers.host + req.url,
+        method: req.method,
+        date: new Date()
+    });
+}
+
+module.exports = routes;
