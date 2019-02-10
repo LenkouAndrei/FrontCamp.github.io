d627ad07c21a36812c4aa3369b57e4cd24d331b2
AndyArt <lenkov.andrei.89@gmail.com>
2019-02-10 12:57:47 +0300

    feature/ change edit page headline depending on link place

--- fc-angular2/src/fc-edit-page/fc-edit-page.ts
+++ fc-angular2/src/fc-edit-page/fc-edit-page.ts
@@ -1,14 +1,23 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'fc-edit-page',
   templateUrl: './fc-edit-page.html',
   styleUrls: ['./fc-edit-page.less'],
 })
-export class FcEditPageComponent {
+export class FcEditPageComponent implements OnInit {
+  public pageName: string;
 
-  constructor(private router: Router){}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  public ngOnInit(): void {
+    this.route.queryParams
+      .subscribe(params => this.pageName = params.pageName);
+  }
 
   public saveAndLeavePage(): void {
     console.log('Saved!!!');
